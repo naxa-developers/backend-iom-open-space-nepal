7fe2c5c7c84f9cbebb314127f207e4e4345d4b62
SandipNup <sandipneupane65@gmail.com>
2019-11-05 11:04:55 +0545

    front page api and models

--- iom/api/urls.py
+++ iom/api/urls.py
@@ -2,10 +2,14 @@ from rest_framework.routers import DefaultRouter
 from api.viewsets import core_viewsets
 from django.urls import path, include
 from api.viewsets import core_viewsets
+from api.viewsets import front_viewset
 
 router = DefaultRouter()
 router.register(r'slider', core_viewsets.SliderViewSet)
 router.register(r'identify_open_space', core_viewsets.CreateOpenSpaceViewSet)
+router.register(r'header', front_viewset.HeaderViewSet)
+router.register(r'open_space_ide', front_viewset.OpenSpaceIdeViewSet)
+router.register(r'open_space_def', front_viewset.OpenSpaceDefViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
--- iom/front/views.py
+++ iom/front/views.py
@@ -1,4 +1,3 @@
 from django.shortcuts import render
-import csv
 
 # Create your views here.
