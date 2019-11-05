66ed106e282ca7b1cd929eb8c51fb641fd6b85d1
SandipNup <sandipneupane65@gmail.com>
2019-11-05 11:38:22 +0545

    registered all models on admin site

--- iom/core/admin.py
+++ iom/core/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from core.models import Slider, Province, District, Municipality, Ward,\
+    SuggestedUse, Services, Question, OpenSpace, Report, CreateOpenSpace,\
+    NearbyAmenities, EducationFacility, HealthFacility
 
 # Register your models here.
+
+admin.site.register(Slider)
+admin.site.register(District)
+admin.site.register(Municipality)
+admin.site.register(Ward)
+admin.site.register(SuggestedUse)
+admin.site.register(Services)
+admin.site.register(Question)
+admin.site.register(OpenSpace)
+admin.site.register(Report)
+admin.site.register(CreateOpenSpace)
+admin.site.register(NearbyAmenities)
+admin.site.register(EducationFacility)
+admin.site.register(HealthFacility)
