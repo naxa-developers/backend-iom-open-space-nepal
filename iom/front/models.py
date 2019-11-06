1f6aba59fa9b8aefcc2069ef6a4b0969f50b1b33
SandipNup <sandipneupane65@gmail.com>
2019-11-06 15:48:45 +0545

    v

--- iom/front/models.py
+++ iom/front/models.py
@@ -13,14 +13,12 @@ class Header(models.Model):
         return self.title
 
 
-
 class OpenSpaceDef(models.Model):
     title = models.TextField(blank=True, null=True)
     title_nep = models.TextField(blank=True, null=True)
     description = models.TextField(blank=True, null=True)
     description_nep = models.TextField(blank=True, null=True)
-    video = models.FileField(upload_to='open_space_def',
-                             blank=True, null=True)
+    video = models.URLField(blank=True,null=True)
 
     def __str__(self):
         return self.title
