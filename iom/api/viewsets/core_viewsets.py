560c582b9569c1f13e7e257f69bf7104900752b0
SandipNup <sandipneupane65@gmail.com>
2019-11-11 13:13:22 +0545

    province district and municipality api

--- iom/api/viewsets/core_viewsets.py
+++ iom/api/viewsets/core_viewsets.py
@@ -7,6 +7,7 @@ from api.serializers.core_serializers import SliderSerializer,\
     ServiceSerializer, QuestionSerializer, OpenSpaceSerializer,\
     ReportSerializer
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
 
 
@@ -102,5 +103,99 @@ def dummy_api_view(request):
 
     }
     return Response({"data": data})
+
+
+class OpenSpaceLandingApi(APIView):
+    authentication_classes = []
+    permission_classes = []
+
+    def get(self, request):
+        data = []
+        open_spaces = OpenSpace.objects.all()
+        for open_space in open_spaces:
+            data.append(
+                {
+                    "title": open_space.title,
+                    "province": open_space.province,
+                    "district": open_space.district,
+                    "municipality": open_space.municipality,
+                    "address": open_space.address,
+                    "image": open_space.image.url,
+                    "latitude": open_space.latitude,
+                    "longitude": open_space.longitude
+                }
+            )
+
+        return Response({"data": data})
+
+
+class DistrictApi(APIView):
+    authentication_classes = []
+    permission_classes = []
+
+    def get(self, request):
+        data = []
+        districts = District.objects.all()
+
+        for district in districts:
+            data.append(
+                {
+                    "id": district.id,
+                    "name": district.name
+                }
+            )
+
+        return Response({"data": data})
+
+
+class MunicipalityApi(APIView):
+    authentication_classes = []
+    permission_classes = []
+
+    def get(self, request):
+        data = []
+        municipalities = Municipality.objects.all()
+
+        for municipality in municipalities:
+            data.append(
+                {
+                    "id": municipality.id,
+                    "name": municipality.name
+                }
+            )
+
+        return Response({"data": data})
+
+
+class ProvinceApi(APIView):
+    authentication_classes = []
+    permission_classes = []
+
+    def get(self, request):
+        data = []
+        provinces = Province.objects.all()
+
+        for province in provinces:
+            data.append(
+                {
+                    "id": province.id,
+                    "name": province.name
+                }
+            )
+
+        return Response({"data": data})
+
+
+
+
+# class GlimpseOfOpenSpace(APIView):
+#     authentication_classes = []
+#     permission_classes = []
+#
+#     def get(self):
+#         open_space = OpenSpace.objects.all()
+
+
+
 
 
