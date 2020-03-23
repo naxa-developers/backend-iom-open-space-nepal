dbc758218a62d239952f2bbb89ef147eddfade65
SandipNup <sandipneupane65@gmail.com>
2020-03-23 14:57:45 +0545

    glimse api

--- core/models.py
+++ core/models.py
@@ -308,6 +308,7 @@ class Report(models.Model):
     reply = models.TextField(null=True, blank=True)
     token = models.CharField(max_length=500, null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    is_published = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
