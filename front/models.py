7e3eb241ddc3fccb594faf6aa3544d5b5d62d077
SandipNup <sandipneupane65@gmail.com>
2020-04-01 09:41:20 +0545

    about header fields chaned

--- front/models.py
+++ front/models.py
@@ -67,7 +67,9 @@ class OpenSpaceApp(models.Model):
 class AboutHeader(models.Model):
     title = models.CharField(max_length=100, blank=True, null=True)
     title_nep = models.CharField(max_length=100, blank=True, null=True)
-    url = models.CharField(max_length=100, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    description_nep = models.TextField(blank=True, null=True)
+    image = models.ImageField(blank=True, null=True, upload_to='front')
 
     def __str__(self):
         return self.title
